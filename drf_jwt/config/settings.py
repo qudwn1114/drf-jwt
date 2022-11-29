9b7ae354d466cd199cd302819c5f6441740515b3
qudwn1114 <qudwn1114@gmail.com>
2022-11-30 01:38:12 +0900

    fix: settings.py

--- drf_jwt/config/settings.py
+++ drf_jwt/config/settings.py
@@ -35,6 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #restframework
+    'rest_framework',
+    'rest_framework_simplejwt',
+	'rest_framework_simplejwt.token_blacklist',
+
     'api',
 ]
 
